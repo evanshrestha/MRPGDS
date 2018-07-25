31a1dcfef7e2b4c460e304f65e8427aa0bb3b8b5
Jordan Bailey <jbailey.day@gmail.com>
2018-07-24 20:33:35 -0400

    picked update method

--- main/game.py
+++ main/game.py
@@ -11,16 +11,17 @@ import pygame
 # @evan hit me up I have a question about defining variables then instaintiating them
 
 #pre-defined variables
-clock = pygame.time.Clock()
+clock = None
 
 #global variables
 tickrate = 20
 framerate = 60 # 0 = unlimited
-running = 1
+running = False
 
 def init():
     #pseudo-contructor
     pygame.init()
+    clock = pygame.time.Clock()
 
 def update(delta): pass
     # entry point for all loop logic
@@ -32,21 +33,8 @@ def main():
 
     init()
 
-
     # mainloop
     while running:
-        # TODO choose update method
-        '''
-        current:
-            time passed since last update call [delta time] and
-            use it as a modifier for movement, etc
-        alternative:
-            limit the rate in which the update function is called
-            e.g. call the method every X milliseconds
-
-        both have inaccuracy due to the clock object limitations,
-        however not sure which would be more accurate / efficent
-        '''
         update(clock.get_time())
 
         # render method ran based on clock
