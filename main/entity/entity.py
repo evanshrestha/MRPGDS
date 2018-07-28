3f6ee4b9cc3b57cf12990cb5f002dd2de8d67321
Unknown <Evanshrestha@gmail.com>
2018-07-27 20:15:25 -0400

    Added entity class
    (definitely needs to be changed to however we want to abstract things)

---
+++ main/entity/entity.py
@@ -0,0 +1,15 @@
+from graphics.screen import Screen
+
+class Entity():
+
+'''
+not sure how much you wanna abstract here
+or how you wanna do this so change it
+'''
+
+# probably should change this to whatever you wanna abstract
+    def __init__(x = 0, y = 0, width = 0, height = 0):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
