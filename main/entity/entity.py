0eab9c37f3e9832e635b2d547b0835a94629718b
Unknown <Evanshrestha@gmail.com>
2018-07-28 23:26:16 -0400

    Added screen as input parameter for rendering

--- main/entity/entity.py
+++ main/entity/entity.py
@@ -1,20 +1,19 @@
-from graphics.screen import Screen
-
-class Entity():
 
 '''
 not sure how much you wanna abstract here
 or how you wanna do this so change it
 '''
 
+class Entity():
+
 # probably should change this to whatever you wanna abstract
-    def __init__(self, pos = (0, 0), size = (0, 0)):
+    def __init__(self, pos = (0, 0), size = (0, 0), screen = None):
         self.pos = pos
         self.size = size
+        self.screen = screen
 
     def render(self):
         pass
 
     def update(self):
         pass
-    
