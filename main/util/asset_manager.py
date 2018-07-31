7037370968b27d27382439237e0a308fde889866
Unknown <Evanshrestha@gmail.com>
2018-07-31 16:57:11 -0400

    Fixed cross-platform file loading again
    Maybe

--- main/util/asset_manager.py
+++ main/util/asset_manager.py
@@ -7,4 +7,7 @@ import os
 # rn this is just a shortcut, but if we add imagemaps (is that the word?)
 # then we can change it to get a certain section of the loaded image
 def load_image(image_path):
-    return pygame.image.load(os.path.join("data", "../res/" + image_path))
+    path_prefix = ""
+    if os.name == "nt":
+        path_prefix = "data"
+    return pygame.image.load(os.path.join(path_prefix, "../res/" + image_path))
